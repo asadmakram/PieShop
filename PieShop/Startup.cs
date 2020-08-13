e1887f10fb76c2a462035adb506adf490e376889
Asad Mehmood Akram <asadmakram@gmail.com>
2020-08-13 21:50:16 +0000

    modification in migration

--- PieShop/Startup.cs
+++ PieShop/Startup.cs
@@ -33,6 +33,11 @@ namespace PieShop
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            using (var scope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                scope.ServiceProvider.GetRequiredService<AppDBContext>().Database.Migrate();
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
