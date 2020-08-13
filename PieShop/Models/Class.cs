172bdd20cf0406cb055d8172586b9eee9973a4e1
Asad Mehmood Akram <asadmakram@gmail.com>
2020-08-13 21:07:06 +0000

    Initial Commit

---
+++ PieShop/Models/Class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDBContext _appDBContext = null;
+        public CategoryRepository(AppDBContext appDBContext)
+        {
+            _appDBContext = appDBContext;
+        }
+
+        public IEnumerable<Category> AllCategories => _appDBContext.Categories;
+    }
+}
---
+++ PieShop/Models/PieRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly AppDBContext _appDBContext = null;
+        public PieRepository(AppDBContext appDBContext)
+        {
+            _appDBContext = appDBContext;
+        }
+        public IEnumerable<Pie> AllPies => _appDBContext.Pies.Include(c => c.Category);
+
+        public IEnumerable<Pie> PiesOfTheWeek
+        {
+            get
+            {
+                return _appDBContext.Pies.Include(c => c.Category).Where(x => x.IsPieOfTheWeek);
+            }
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return _appDBContext.Pies.Include(c=>c.Category).FirstOrDefault(x => x.PieId == pieId);
+        }
+    }
+}
